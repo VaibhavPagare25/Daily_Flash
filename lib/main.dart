885d36e7def4043965b650d6640abbf9f8cbffa6
VaibhavPagare25 <vaibhavpagare1234@gmail.com>
2024-03-08 08:58:58 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,11 @@ import 'package:flash/Flash05/flash05_1.dart';
 import 'package:flash/Flash05/flash05_2.dart';
 import 'package:flash/Flash05/flash05_3.dart';
 import 'package:flash/Flash05/flash05_4.dart';
+import 'package:flash/Flash06/flash06_1.dart';
+import 'package:flash/Flash06/flash06_2.dart';
+import 'package:flash/Flash06/flash06_3.dart';
+import 'package:flash/Flash06/flash06_4.dart';
+import 'package:flash/Flash06/flash06_5.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +21,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: FlashFive4(),
+      home: FlashSix5(),
     );
   }
 }
